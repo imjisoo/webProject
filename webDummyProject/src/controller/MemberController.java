bc09c38aa3a59585d2f515ebb66354ba36b17b3d
imjisoo <imjisoo0514@gmail.com>
2017-06-29 18:05:39 +0900

    commit

--- webDummyProject/src/controller/MemberController.java
+++ webDummyProject/src/controller/MemberController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package controller;
 
 import java.util.Map;
@@ -8,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import vo.BookVO;
 
 @Controller
 public class MemberController {
@@ -27,35 +28,3 @@ public class MemberController {
 	}
 	
 }
-=======
-package controller;
-
-import java.util.Map;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
-import vo.MemberVO;
-
-@Controller
-public class MemberController {
-	
-	@RequestMapping("/joinForm")
-	public String joinForm () {
-		
-		return "join_form";
-		
-	}
-	
-	@RequestMapping(value="/join", method=RequestMethod.POST)
-	public ModelAndView join (@RequestParam Map<String, Object> conditions) {
-		
-		return null;
-		
-	}
-	
-}
->>>>>>> 6b1425cbe40c7ebb1a97764a3f41948ae537a574
