7134a5ab446653ac25db2f78d5de1e88082cd6c2
imjisoo <imjisoo0514@gmail.com>
2017-06-29 16:25:17 +0900

    test

---
+++ webDummyProject/src/controller/MemberController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberController {
+	
+	@RequestMapping("/joinForm")
+	public String joinForm () {
+		
+		return "join_form";
+		
+	}
+	
+}
